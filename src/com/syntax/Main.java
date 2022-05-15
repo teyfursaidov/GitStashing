4c870ccfcc7e229ff63f0c0dcb6475bfc0d2a50a
teyfursaidov <teyfursaidov1991@gmail.com>
2022-05-15 19:05:12 +0300

    Red car is ready with some basic features.

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -7,6 +7,12 @@ public class Main {
         // write your code here
         BasicFeatures car = new BasicFeatures();
 
+        car.color = "Black";
+        car.doors = "3";
+        car.engineCapacity = "1486cc";
+        car.headlights = "Halogen";
+        car.regNo = "NV448";
+
 
     }
 }
